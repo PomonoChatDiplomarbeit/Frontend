260f0fde77eb9a581be4932e782bda5ce7313e01
fabian.tschofenig <tschofe18@gmail.com>
2017-07-28 11:20:21 +0200

    sending messages without server working

---
+++ src/main-page/messageObj.js
@@ -0,0 +1,7 @@
+class MessageObj{
+        constructor(text, sender, receiver) {
+            this.text = text;
+            this.sender = sender;
+            this.receiver = receiver;
+        }
+    }
--- src/main-page/serverCommunication.js
+++ src/main-page/serverCommunication.js
@@ -1,8 +1,50 @@
 class Communicator {
-    static getMessagesForChat(chatPartnerName) {
-        console.log('partner: ' + chatPartnerName);
+    constructor(){
+        this.map=[]
+        this.map=this.loadTestData()
     }
-    static anotherStaticMethod() {
-        return this.staticMethod() + ' from another static method';
+    loadTestData(){
+        var temp = [];
+        temp.push(this.newChat("Dragan"));
+        temp.push(this.newChat("Sandro"));
+        return temp;
+    }
+    getMessagesForChat(chatPartnerName) {
+        console.log('partner: ' + chatPartnerName);
+        return this.map.
+            filter(function (elem) {
+                return elem.partner==chatPartnerName;
+            })[0].listofmessages;
+    }
+    getAllChatPartners() {
+        return this.map.map(function (item) {
+            return item.partner;
+        })
+
+    }
+    newChat(partner){
+        return {
+            partner: partner,
+            listofmessages: [
+                    new MessageObj("Nachricht 1", partner, "Fabian"),
+                    new MessageObj("Nachricht 2", partner, "Fabian")
+                ]
+        };
+    }
+    emitMessage(message){
+        console.log('emitMessage entered!');
+        var element;
+        for (var i in this.map) {
+            if (this.map[i].partner == message.receiver) {
+                console.log('Message added!');
+                element = this.map[i];
+                this.map.splice(i,1);
+                break; 
+            }
+        }
+        element.listofmessages.push(message);
+        this.map.push(element);
+        //this.map.reverse();
+        console.log(this.map);
     }
 }
