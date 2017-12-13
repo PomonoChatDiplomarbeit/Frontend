6081e877d044a9d503e3c83e423b9451787660bc
fabiantschofenig <tschofe18@gmail.com>
2017-12-13 19:14:25 +0100

    login working

--- src/main-page/allClasses.js
+++ src/main-page/allClasses.js
@@ -7,8 +7,9 @@ class Message {
     }
 }
 class User {
-    constructor(username) {
+    constructor(username, pwd) {
         this.username = username
+        this.pwd = pwd
     }
 }
 class Room {
